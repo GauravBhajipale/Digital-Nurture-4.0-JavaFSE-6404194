0cc43b8c83c9bc2533db5c00856730b929a96caa
GauravBhajipale <bhajipalegaurav11@gmail.com>
2025-06-22 16:13:32 +0530

    Update FinancialForecasting.java

--- Week_1_Algorithms_Data Structures/07_Financial Forecasting/code/FinancialForecasting.java
+++ Week_1_Algorithms_Data Structures/07_Financial Forecasting/code/FinancialForecasting.java
@@ -2,41 +2,32 @@ import java.util.*;
 
 public class FinancialForecasting {
 
-    private static Map<Integer, Double> memo = new HashMap<>();
-
-   
-    public static double calculateFutureValue(double principal, double rate, int years) {
+    public static double predictFutureValue(double principal, double rate, int years) {
         
         if (years == 0) {
             return principal;
         }
 
         
-        if (memo.containsKey(years)) {
-            return memo.get(years);
-        }
-
-        
-        double result = calculateFutureValue(principal * (1 + rate), rate, years - 1);
-        memo.put(years, result);  
+        double result = predictFutureValue(principal * (1 + rate), rate, years - 1); 
         return result;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // User Input
+        // user input
         System.out.print("Enter initial amount: ");
         double principal = scanner.nextDouble();
 
-        System.out.print("Enter annual growth rate (e.g., 0.05 for 5%): ");
+        System.out.print("Enter annual growth rate (ex: 0.05 for 5%): ");
         double rate = scanner.nextDouble();
 
         System.out.print("Enter number of years: ");
         int years = scanner.nextInt();
 
        
-        double futureValue = calculateFutureValue(principal, rate, years);
+        double futureValue = predictFutureValue(principal, rate, years);
 
     
         System.out.printf("Future Value after %d years: %.2f\n", years, futureValue);
