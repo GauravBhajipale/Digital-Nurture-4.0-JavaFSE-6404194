d0c68820b2cddf512623e84c89ca08d859072d4d
GauravBhajipale <bhajipalegaurav11@gmail.com>
2025-06-22 16:14:56 +0530

    Update FinancialForecasting.java

--- Week_1_Algorithms_Data Structures/07_Financial Forecasting/code/FinancialForecasting.java
+++ Week_1_Algorithms_Data Structures/07_Financial Forecasting/code/FinancialForecasting.java
@@ -1,21 +1,15 @@
 import java.util.*;
-
 public class FinancialForecasting {
-
     public static double predictFutureValue(double principal, double rate, int years) {
-        
+
         if (years == 0) {
             return principal;
         }
-
-        
         double result = predictFutureValue(principal * (1 + rate), rate, years - 1); 
         return result;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         // user input
         System.out.print("Enter initial amount: ");
         double principal = scanner.nextDouble();
@@ -25,12 +19,8 @@ public class FinancialForecasting {
 
         System.out.print("Enter number of years: ");
         int years = scanner.nextInt();
-
-       
         double futureValue = predictFutureValue(principal, rate, years);
 
-    
         System.out.printf("Future Value after %d years: %.2f\n", years, futureValue);
-
     }
 }
